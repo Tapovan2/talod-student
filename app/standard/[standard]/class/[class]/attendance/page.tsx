73b3e855d85cb8fbaf9330d1c563553537a3ccdb
Patel Swapnil <info.tapovan2@gmail.com>
2025-07-08 08:27:40 +0530

    fix error attendance

--- app/standard/[standard]/class/[class]/attendance/page.tsx
+++ app/standard/[standard]/class/[class]/attendance/page.tsx
@@ -65,7 +65,7 @@ export default function AttendancePage() {
       adjustedDate.setDate(adjustedDate.getDate() + 1);
 
       const response = await fetch(
-        `https://t1-api-attendance.vercel.app/api/attendance`,
+        `https://talod-api.vercel.app/api/attendance`,
         {
           method: "POST",
           headers: {
@@ -79,13 +79,13 @@ export default function AttendancePage() {
           }),
         }
       );
-      
+
       if (!response.ok) {
         throw new Error("Failed to mark attendance");
       }
       const data = await response.json();
-      console.log("data",data);
-      
+      console.log("data", data);
+
       if (data.success) {
         alert("Attendance marked successfully");
         toast({
