ee0e55ff552962b0c1d71be38a5bac1ba3707961
Patel Swapnil <info.tapovan2@gmail.com>
2025-07-18 14:09:45 +0530

    performance && excel fix

--- app/api/mark-entries/route.ts
+++ app/api/mark-entries/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
     const where: Record<string, any> = {};
 
     if (standard) {
-      where.standard = (standard);
+      where.standard = parseInt(standard);
     }
 
     if (className) {
